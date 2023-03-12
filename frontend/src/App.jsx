d283bf56fe8f77497dc5457e0f8fc93b26d2ea5c
darenbt <darenbt@gmail.com>
2023-03-12 03:22:20 -0500

    added add and delete item functionality.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,11 +16,22 @@ function App() {
     <>
 
     <h1>Todo List: </h1>
-    <ul class="todoItems">
+    <ul className="todoItems">
       {items.map((item, index) => (
         <li key={index} className="item">
           {item.completed === true? <span className="completed">{item.thing}</span> : <span>{item.thing}</span>}
-          <span className="fa fa-trash"></span>
+          <span className="fa fa-trash" onClick={async event => {
+            await fetch('/api/deleteItem', {
+              method: 'delete',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                'itemFromJS': item.thing
+              })
+            })
+            
+            setItems(items.filter(loopItem => loopItem._id !== item._id))
+            setLeft(prev => prev -1)
+          }}></span>
         </li>
       ))}
     </ul>
@@ -29,7 +40,22 @@ function App() {
 
     <h2>Add A Todo:</h2>
 
-    <form action="/addTodo" method="POST">
+    <form action="/api/addTodo" method="POST" onSubmit={async event => {
+      event.preventDefault();
+      const form = event.currentTarget
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: JSON.stringify({
+          todoItem: form.elements.todoItem.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const newItem = await response.json()
+      setItems(prev => [...prev, newItem])
+      setLeft(prev => prev += 1)
+    }}>
         <input type="text" placeholder="Thing To Do" name="todoItem" />
         <input type="submit" />
     </form>
