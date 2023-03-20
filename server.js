6d2ec33946d5552c9167ccbd54a02bc913127e59
darenbt <darenbt@gmail.com>
2023-03-20 00:57:26 -0500

    removed view engine.

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         db = client.db(dbName)
     })
     
-app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
